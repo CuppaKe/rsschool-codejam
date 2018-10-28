bd60b855a22bc6e95273e7db01a5a8017651859c
CuppaKe <turnman87@gmail.com>
2018-10-28 16:15:54 +0300

    feat: add function make.js and tests for it

---
+++ src/make.js
@@ -0,0 +1,15 @@
+/**
+ * Function finds sum of integers
+ * @param {} - integers or function
+ * @return {}  resulted sum
+ */
+module.exports = function make() {
+  if (arguments.length === 0) return 0;
+  const array = [];
+  array.push(...arguments);
+  const f = (...arg) => {
+    array.push(...arg);
+    return typeof arg[arg.length - 1] !== 'function' ? f : array.slice(0, -1).reduce(arg[arg.length - 1], 0);
+  };
+  return f;
+}
--- test.js
+++ test.js
@@ -1,15 +1,36 @@
 const assert = require('assert');
 Object.freeze(assert);
 const sumOfOther  = require('./src/sumOfOther');
+const make  = require('./src/make');
 
 describe('sum', () => {
-  it('0', () => {
+  it('1', () => {
     const solution = sumOfOther([2, 3, 4, 1]);
     assert.deepEqual(solution, [8, 7, 6, 9]);
   });
 
-  it('1', () => {
+  it('2', () => {
     const solution = sumOfOther([]);
     assert.deepEqual(solution, []);
   }); 
+});
+
+describe('makeSum', () => {
+  it('1', () => {
+    const sum = (a, b) => a + b;
+    const solution = make(15)(34, 21, 666)(41)(sum);
+    assert.deepEqual(solution, 777);
+  });
+
+  it('2',  () => {
+    const solution = make();
+    assert.deepEqual(solution, 0);
+  });
+  
+  it('3', () => {
+    const sum = (a, b) => a + b;
+    const solution = make(15)(34, 21, 666)(41)(23, sum);
+    assert.deepEqual(solution, 800);
+  });
 })
+
