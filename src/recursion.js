f1e346811cd15fa7c874f134beb4356daabdb0a7
CuppaKe <turnman87@gmail.com>
2018-10-28 16:33:10 +0300

    feat: add file for recursion function and tests for it

---
+++ src/recursion.js
@@ -0,0 +1,8 @@
+/**
+ * Function converts tree object to array
+ * @param {Oject} tree - tree as an object
+ * @return {}  array
+ */
+module.exports = function recursion(tree) {
+
+}
--- test.js
+++ test.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 Object.freeze(assert);
 const sumOfOther  = require('./src/sumOfOther');
 const make  = require('./src/make');
+const recursion  = require('./src/recursion');
 
 describe('sum', () => {
   it('1', () => {
@@ -32,5 +33,13 @@ describe('makeSum', () => {
     const solution = make(15)(34, 21, 666)(41)(23, sum);
     assert.deepEqual(solution, 800);
   });
-})
+});
+
+describe('recursion', () => {
+  it('1', () => {
+    const tree = {"value":100,"left":{"value":90,"left":{"value":70},"right":{"value":99}},"right":{"value":120,"left":{"value":110},"right":{"value":130}}};
+    const solution = recursion(tree);
+    assert.deepEqual(solution, [[100], [90, 120], [70,99,110,130]]);
+  });
+});
 
