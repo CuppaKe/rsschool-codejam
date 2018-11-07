9cd576798e6e035e3da9bb6f1e6dfb28302099d2
CuppaKe <turnman87@gmail.com>
2018-11-07 21:16:22 +0300

    feat: add function recursion

--- src/recursion.js
+++ src/recursion.js
@@ -4,5 +4,21 @@
  * @return {}  array
  */
 module.exports = function recursion(tree) {
-
-}
+  const res = [];
+  let count = 0;
+  function rec(obj) {
+    res[count] ? res[count].push(obj.value) : res[count] = [obj.value];
+    if (obj.left) {
+      count++;
+      rec(obj.left);
+      count--;
+    }
+    if (obj.right) {
+      count++;
+      rec(obj.right);
+      count--;
+    }
+  }
+  rec(tree);
+  return res;
+};
