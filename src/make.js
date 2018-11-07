296cbac7ef601dae4fd2290fbdbd8f82170bfe2a
CuppaKe <turnman87@gmail.com>
2018-11-07 20:38:47 +0300

    refactor: change arguments to rest parameters

--- src/make.js
+++ src/make.js
@@ -3,10 +3,10 @@
  * @param {} - integers or function
  * @return {}  resulted sum
  */
-module.exports = function make() {
-  if (arguments.length === 0) return 0;
+module.exports = function make(...args) {
+  if (args.length === 0) return 0;
   const array = [];
-  array.push(...arguments);
+  array.push(...args);
   const f = (...arg) => {
     array.push(...arg);
     return typeof arg[arg.length - 1] !== 'function' ? f : array.slice(0, -1).reduce(arg[arg.length - 1]);
